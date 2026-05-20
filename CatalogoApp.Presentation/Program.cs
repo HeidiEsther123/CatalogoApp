9cd98a00742ded7f27f6055222fbe9e92f1a0f4e
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-05-20 11:21:51 -0600

    FIX: bug fixes

--- CatalogoApp.Presentation/Program.cs
+++ CatalogoApp.Presentation/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddScoped<ItemService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
