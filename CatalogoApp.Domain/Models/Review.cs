071921a3e08ceceed7fb0f6d9628848c110d8ce9
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-05-22 22:32:38 -0600

    CREATE: Creando Review

---
+++ CatalogoApp.Domain/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace CatalogoApp.Domain.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; } 
+        public string Usuario { get; set; }
+        public int Calificacion { get; set; } 
+        public string Comentario { get; set; }
+    }
+}
